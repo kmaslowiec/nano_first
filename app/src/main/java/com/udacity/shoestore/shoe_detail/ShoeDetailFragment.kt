2dd47663afaa625fddce17f2c89fc217a8af3e6f
Konrad <kmaslowiec@gmail.com>
2021-11-07 02:06:50 +0100

    Cancel button listener

--- app/src/main/java/com/udacity/shoestore/shoe_detail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoe_detail/ShoeDetailFragment.kt
@@ -1,19 +1,32 @@
 package com.udacity.shoestore.shoe_detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
 class ShoeDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_detail, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentShoeDetailBinding>(layoutInflater, R.layout.fragment_shoe_detail, container, false)
+
+        binding.buttonCancel.setOnClickListener { view ->
+            view.findNavController().navigate(
+                ShoeDetailFragmentDirections
+                    .actionShoeDetailFragmentToShoeListFragment()
+            )
+        }
+
+        return binding.root
     }
 }
